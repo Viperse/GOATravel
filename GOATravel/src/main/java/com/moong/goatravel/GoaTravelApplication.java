b1226ae960b39f675d8130922dbb3abd09356f6f
Viperse <129846485+Viperse@users.noreply.github.com>
2024-05-11 16:41:06 +0900

    feat 프로젝트 생성
    작업자: 김도경
    작업 내용: spring boot 프로젝트 생성

---
+++ GOATravel/src/main/java/com/moong/goatravel/GoaTravelApplication.java
@@ -0,0 +1,13 @@
+package com.moong.goatravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoaTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoaTravelApplication.class, args);
+	}
+
+}
